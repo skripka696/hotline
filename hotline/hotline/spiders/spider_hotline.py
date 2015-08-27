f6650c90a0b079cdc022f537ea358ff6531c65a7
skripka696 <skripka696@ukr.net>
2015-08-27 17:48:22 +0300

    name [description]

--- hotline/hotline/spiders/spider_hotline.py
+++ hotline/hotline/spiders/spider_hotline.py
@@ -44,37 +44,28 @@ class HotlineSpider(CrawlSpider):
     # #     print item
     # #     return item
     rules = [
-        Rule
-        (
-            LinkExtractor(
-                restrict_xpaths=("//*[@id='catalogue']/div/span/a")),
-            callback = 'parse_item',
-            follow = True,
-        ),
+        Rule(LinkExtractor(restrict_xpaths=("//*[@id='catalogue']/ul/li/div/div/div/a", )),  # "//*[@id='catalogue']/div/span/a",
+             callback='parse_item', follow=True),
 
-        Rule
-        (
-            LinkExtractor(
-                restrict_xpaths=("//*[@id='catalogue']/ul/li/div/div/div/a")),
-            callback = 'parse_description',
-            follow = True,
-        )
+        # Rule(LinkExtractor(
+        #     restrict_xpaths=("//*[@id='catalogue']/ul/li/div/div/div/a", )),
+        #     callback='parse_description', follow=True)
     ]
 
     def parse_item(self, response):
         hxs = response
         item = HotlineItem()
-        item['name'] = hxs.xpath('//*[@id="catalogue"]/ul/li/div/div/div/a/text()').extract()[0]
+        item['name'] = hxs.xpath('/html/body/div/div/div/div/h1/text()').extract()[0].strip()
         print item['name']
+        item['description'] = hxs.xpath('/html/body/div/div/div/div/div/div/div/div/p/text()').extract()[0].strip()
         yield item
 
-    def parse_description(self, response):
-        hxs = response
-        item = HotlineItem()
-
-        item['description'] = hxs.xpath('/html/body/div/div/div/div/div/div/div/div/p/text()').extract()
-        print type(hxs.xpath('/html/body/div/div/div/div/div/div/div/div/p/text()').extract()[0])
-        yield item
-
-
-
+    # def parse_description(self, response):
+    #     hxs = response
+    #     item = HotlineItem()
+    #
+    #     item['description'] = hxs.xpath(
+    #         '/html/body/div/div/div/div/div/div/div/div/p/text()').extract()
+    #     print type(hxs.xpath(
+    #         '/html/body/div/div/div/div/div/div/div/div/p/text()').extract()[0])
+    #     yield item
